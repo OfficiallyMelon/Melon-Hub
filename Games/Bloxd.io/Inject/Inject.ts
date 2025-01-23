6106b2d3aae7bfbd778e33101c3c8c2aa1c2284a
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2025-01-23 22:52:38 +1000

    Create Inject.ts

---
+++ Games/Bloxd.io/Inject/Inject.ts
@@ -0,0 +1,65 @@
+interface Config {
+    coordinates: [number | null, number | null, number | null];
+    methods: Record<string, unknown>;
+    hookedObject?: any;
+    injectFunctionality?: boolean;
+    babylonEngine?: any;
+    noaInstance?: any;
+    genericPlayerState?: any;
+    CurrentlyInjected: boolean;
+    freecamPosition: [x: number, y: number, z: number];
+}
+
+const config: Config = {
+    coordinates: [null, null, null],
+    methods: {},
+    CurrentlyInjected: false,
+    freecamPosition: [0,0,0]
+};
+
+// Functions
+function hookObjectAssign(target: any, prop: string, handler: (...args: any[]) => void) {
+    const originalMethod = target[prop];
+    target[prop] = new Proxy(originalMethod, {
+        apply: function(originalFunc, thisArg, argsList) {
+            handler(...argsList);
+            return Reflect.apply(originalFunc, thisArg, argsList);
+        }
+    });
+}
+
+// Hook Noa (thanks blackhole for injection method, yay!)
+hookObjectAssign(Object, 'assign', function(...params: any[]) {
+    let hasSwingTime = false;
+    try {
+        if (params[2].swingDuration !== undefined) {
+            hasSwingTime = true;
+        }
+    } catch (err) {
+        // Property swingDuration not found
+    }
+
+    let hasIdentifier = false;
+    try {
+        if (params[1].__id !== undefined) {
+            hasIdentifier = true;
+        }
+    } catch (err) {
+        // Property __id not found
+    }
+
+    if (hasSwingTime && hasIdentifier && params.length === 4 && params[2].swingDuration === 200 && params[1].__id === 1) {
+        config.hookedObject = params[0];
+        console.log('Injection Completed');
+        setTimeout(function() {
+            config.babylonEngine = (window as any).BABYLON;
+            config.noaInstance = config.hookedObject.heldItem.noa;
+            console.log('Hooked Noa: ', config.noaInstance);
+            config.CurrentlyInjected = true;
+            console.log("Currently Injected: ", config.CurrentlyInjected);
+            config.genericPlayerState = config.noaInstance.ents._storage.genericLifeForm.hash[config.noaInstance.playerEntity];
+        }, 1);
+    }
+});
+
+export { config };
